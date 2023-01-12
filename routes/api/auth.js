ac9f9239daddc6e6bd2d816120e04ad9cbb45f1a
wawk <white_knight@prodigy.net>
2023-01-12 08:55:55 -0500

    added middleware for jsonwebtoken verification

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,74 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcryptjs');
 
 //  @route GET api/auth
 // @desc Test route
 // @access Public
 
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+	try {
+		const user = await User.findById(req.user.id).select('-password');
+		res.json(user);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server error');
+	}
+});
+
+// @route POST api/auth
+// @desc Authenticate User and Get Token
+// @access Public
+
+router.post(
+	'/',
+	[
+		check('name', 'Name is required').not().isEmpty(),
+		check('email', 'Please include a valid Email').isEmail(),
+		check('password', 'A password is required').exists(),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { email, password } = req.body;
+		try {
+			// See if user exists
+			let user = await User.findOne({ email });
+
+			if (!user) {
+				return res.status(400).json({
+					errors: [{ msg: 'Invalid Credentials' }],
+				});
+			}
+
+			// return jsonwebtoken
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+			jwt.sign(
+				payload,
+				config.get('jwtToken'),
+				{ expiresIn: 3600 },
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server error');
+		}
+	}
+);
 
 module.exports = router;
