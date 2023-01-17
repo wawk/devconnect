fc5244821dd5d7f01c3a26f8ba331e34d5af04bf
wawk <white_knight@prodigy.net>
2023-01-17 08:33:43 -0500

    update profile js file to include posting routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -43,6 +43,66 @@ router.post(
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
 		}
+
+		const {
+			company,
+			website,
+			location,
+			bio,
+			status,
+			githubusername,
+			skills,
+			youtube,
+			facebook,
+			twitter,
+			instagram,
+			linkedin,
+		} = req.body;
+
+		//build profile object
+		const profileFields = {};
+
+		profileFields.user = req.user.id;
+		if (company) profileFields.company = company;
+
+		if (website) profileFields.website = website;
+		if (location) profileFields.location = location;
+		if (bio) profileFields.bio = bio;
+		if (status) profileFields.status = status;
+		if (githubusername) profileFields.githubusername = githubusername;
+		if (skills) {
+			profileFields.skills = skills
+				.split(',')
+				.map((skill) => skill.trim());
+		}
+
+		// Build social object
+		profileFields.social = {};
+		if (youtube) profileFields.social.youtube = youtube;
+		if (facebook) profileFields.social.facebook = facebook;
+		if (twitter) profileFields.social.twitter = twitter;
+		if (instagram) profileFields.social.instagram = instagram;
+		if (linkedin) profileFields.social.twitterlinkedin = linkedin;
+
+		try {
+			let profile = await Profile.findOne({ user: req.user.id });
+			if (profile) {
+				//update
+				profile = await profile.findOneAndUpdate(
+					{ user: req.user.id },
+					{ $set: profileFields },
+					{ new: true }
+				);
+				return res.json(profile);
+			}
+			// Create
+			profile = new Profile(profileFields);
+			await Profile.ave();
+			res.json(profile);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error');
+		}
 	}
 );
 
