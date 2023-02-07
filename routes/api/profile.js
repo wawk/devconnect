a5bbd146917b6070ac4ed07d1fbfefe2f63946ad
wawk <white_knight@prodigy.net>
2023-02-07 09:49:12 -0500

    Added route to GET github profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,8 @@ const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const request = require('request');
+const conf = require('config');
 
 //@route GET api/profile/me
 //@desc Get current users profile
@@ -280,7 +282,36 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 		profile.education.splice(removeIndex, 1);
 		await profile.save();
 		res.json(profile);
-	} catch (err) {}
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
+//@route Get api/profile/github/:usearname
+//@desc Get user repos from Github
+//@access Public
+router.get('/github/:username', (req, res) => {
+	try {
+		const options = {
+			uri: `https://api.github.com/users/${
+				req.params.username
+			}/repos?per_page=5&sort=created:asc&client_id=${conf.get(
+				'githubClientId'
+			)}&client_secret=${conf.get('githubSecret')}`,
+			method: 'GET',
+			headers: { 'user-agent': 'node.js' },
+		};
+		request(options, (error, response, body) => {
+			if (error) console.error(error);
+			if (response.statusCode != 200) {
+				res.status(404).json({ msg: 'No Github profile found' });
+			}
+			res.json(JSON.parse(body));
+		});
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error in Get Route for github profile');
+	}
 });
 
 module.exports = router;
